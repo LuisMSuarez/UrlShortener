fc1b146e7948832571b7eb3e40567414ca6ab60d
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-07-29 20:31:27 -0700

    Api stubs (#1)
    * Api stubs
    
    * Finished adding remaining stubs

---
+++ UrlShortenerApi/Controllers/UrlController.cs
@@ -0,0 +1,88 @@
+﻿namespace UrlShortenerApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using UrlShortenerApi.Contracts;
+
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class UrlsController : ControllerBase
+    {
+        public static IList<UrlShortcut> Shortcuts { get; set; } = new List<UrlShortcut>
+        {
+            new UrlShortcut
+            {
+                Shortcut = "xyz",
+                Url = "https://gamershub.azurewebsites.net"
+            }
+        };
+
+        // GET: v1/<UrlController>
+        [HttpGet]
+        public IEnumerable<UrlShortcut> Get()
+        {
+            return Shortcuts;
+        }
+
+        // GET v1/<UrlController>/xyz
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var shortcut = Shortcuts.FirstOrDefault( s => string.Equals(s.Shortcut, id, StringComparison.OrdinalIgnoreCase));
+            if (shortcut == null) {
+                return await Task.FromResult(NotFound());
+            }
+
+            return await Task.FromResult(
+                Redirect(shortcut.Url));
+        }
+
+        // POST v1/<UrlController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UrlShortcut value)
+        {
+            var shortcut = Shortcuts.FirstOrDefault(s => string.Equals(s.Shortcut, value.Shortcut, StringComparison.OrdinalIgnoreCase));
+            if (shortcut != null)
+            {
+                return await Task.FromResult(BadRequest());
+            }
+
+            Shortcuts.Add(value);
+            return await Task.FromResult(Created());
+        }
+
+        // PUT v1/<UrlController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] UrlShortcut value)
+        {
+            if (!string.Equals(id, value.Shortcut, StringComparison.OrdinalIgnoreCase))
+            {
+                return await Task.FromResult(BadRequest());
+            }
+
+            var shortcut = Shortcuts.FirstOrDefault(s => string.Equals(s.Shortcut, id, StringComparison.OrdinalIgnoreCase));
+            if (shortcut == null)
+            {
+                return await Task.FromResult(NotFound());
+            }
+
+            Shortcuts.Remove(shortcut);
+            Shortcuts.Add(value);
+            return await Task.FromResult(Ok());
+        }
+
+        // DELETE v1/<UrlController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var shortcut = Shortcuts.FirstOrDefault(s => string.Equals(s.Shortcut, id, StringComparison.OrdinalIgnoreCase));
+            if (shortcut == null)
+            {
+                return await Task.FromResult(NotFound());
+            }
+
+            Shortcuts.Remove(shortcut);
+            return await Task.FromResult(Ok());
+        }
+    }
+}
