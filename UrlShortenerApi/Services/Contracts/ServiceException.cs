6a8d1708af5aa7416d09b324ac84626411945dc7
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-08-03 09:56:42 -0700

    Service Exceptions and handling in Controller

---
+++ UrlShortenerApi/Services/Contracts/ServiceException.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortenerApi.Services.Contracts
+{
+    public class ServiceException(ServiceResultCode resultCode, string? message = null, Exception? innerException = null)
+        : Exception(message, innerException)
+    {
+        public ServiceResultCode ResultCode { get; } = resultCode;
+    }
+}
